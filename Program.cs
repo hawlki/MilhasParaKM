fbccd224dd2ae647852b4a9ebd698d3a0f731676
israel lopes <israel.lopes2710@gmail.com>
2021-03-21 16:37:36 -0300

    organiza arquivo

--- Program.cs
+++ Program.cs
@@ -8,24 +8,29 @@ namespace MilhasParaKM
         {
             Console.Clear();
             Console.Beep();
+
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Etec Adolpho Berezin");
             Console.WriteLine("Feito por Israel Camilo Lopes");
             Console.WriteLine("1I3");
             Console.WriteLine("-----------------------------");
+
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Write("Digite uma medida em milhas: ");
             String medida = Console.ReadLine();
             Double milha = Convert.ToDouble(medida);
             Double quilometros = milha * 1.609;
+
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{milha}mi é igual a {quilometros}km");
             Console.WriteLine();
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Clique em uma tecla para finalizar este programa");
             Console.ReadKey();
+            
             Console.Clear();
         }
     }
